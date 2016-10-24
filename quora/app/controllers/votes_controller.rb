7abbc850b265da518090984b19555913157189a0
codea_mac_04 <codea_mac_04@gmail.com>
2016-10-24 17:35:19 -0500

    New method to increase votes with ajax

--- quora/app/controllers/votes_controller.rb
+++ quora/app/controllers/votes_controller.rb
@@ -2,4 +2,23 @@ class VotesController < ApplicationController
   def show
     
   end
+
+  def new
+
+    @vote = Vote.create
+    if params[:type] == "Question"
+      @elem = Question.find(params[:id])
+    elsif params[:type] == "Answer"
+      @elem = Answer.find(params[:id])
+    end
+    if @elem
+      @elem.votes << @vote
+      current_user.votes << @vote
+    end
+
+     unless @vote.save
+        flash[:error] = "Vote not saved in database"
+     end 
+  end
+
 end
--- quora/db/migrate/20161020150412_create_votes.rb
+++ quora/db/migrate/20161020150412_create_votes.rb
@@ -2,8 +2,8 @@ class CreateVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :votes do |t|
       t.belongs_to :user 
-      t.belongs_to :question
-      t.belongs_to :answer
+      t.belongs_to :question, null: true
+      t.belongs_to :answer, null: true
       t.timestamps
     end
   end
